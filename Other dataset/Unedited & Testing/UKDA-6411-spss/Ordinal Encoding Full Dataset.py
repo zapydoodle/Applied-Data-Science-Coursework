a519dd96794549c4045f20aab7ac0bc9867fdb12
zapydoodle <72476451+zapydoodle@users.noreply.github.com>
2023-04-28 15:03:39 +0100

    Added Excel file to map encoded data

--- Other dataset/Unedited & Testing/UKDA-6411-spss/Ordinal Encoding Full Dataset.py
+++ Other dataset/Unedited & Testing/UKDA-6411-spss/Ordinal Encoding Full Dataset.py
@@ -1,21 +1,20 @@
 import pandas as pd
 import numpy as np
 
-df=pd.read_csv("MergedDatasets.csv",usecols=lambda c: not c.startswith('Unnamed:'))
-
-map=pd.DataFrame()
-count=0
-for feature in df.columns:
-    potentialItems = []
-    print(feature)
-    if feature !="Id":
-        currentFeature=df[feature]
-        for item in currentFeature:
-            if (item not in potentialItems):
-                potentialItems.append(item)
-        print(potentialItems)
-        map.insert(count,str(feature),pd.Series(potentialItems))
-        count+=1
-map.to_csv("Ordinal Encoding.csv")
+#df=pd.read_csv("MergedDatasets.csv",usecols=lambda c: not c.startswith('Unnamed:'))
+#map=pd.DataFrame()
+#count=0
+#for feature in df.columns:
+#    potentialItems = []
+#    print(feature)
+#    if feature !="Id":
+#        currentFeature=df[feature]
+#        for item in currentFeature:
+#            if (item not in potentialItems):
+#                potentialItems.append(item)
+#        print(potentialItems)
+#        map.insert(count,str(feature),pd.Series(potentialItems))
+#        count+=1
+#map.to_csv("Ordinal Encoding.csv")
 
 
