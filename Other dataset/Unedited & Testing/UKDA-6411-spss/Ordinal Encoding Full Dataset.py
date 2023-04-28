a3017beeac523293479db642fd580fa1d8aeefbc
zapydoodle <72476451+zapydoodle@users.noreply.github.com>
2023-04-28 15:14:44 +0100

    Python Code to encode data test
    Created python file to encode data, tested with sex feature.
    Works with sex feature, need to copy over other features

--- Other dataset/Unedited & Testing/UKDA-6411-spss/Ordinal Encoding Full Dataset.py
+++ Other dataset/Unedited & Testing/UKDA-6411-spss/Ordinal Encoding Full Dataset.py
@@ -17,4 +17,16 @@ import numpy as np
 #        count+=1
 #map.to_csv("Ordinal Encoding.csv")
 
+df=pd.read_csv("MergedDatasets.csv",usecols=lambda c: not c.startswith('Unnamed:'))
+map={"Sex":{"Male":1,"Female":2}
+}
 
+
+for feature in map:
+    currentFeatureMap=map[feature]
+    currentFeature=df[feature]
+    currentFeature=currentFeature.replace(currentFeatureMap)
+    df[feature]=currentFeature
+
+
+df.to_csv("MergedDatasetsEncoded.csv",index=False,encoding='utf-8',na_rep='NaN')
